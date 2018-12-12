df4727bad8e005b018825579e6374f012bb0dfe8
Xmader <xmader@outlook.com>
2018-12-12 21:06:13 +0800

    增加英语(美国)语言包

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# language-en_US
+
+# ZettaForum/language-en_US
+
+> English (United States) Language Pack for ZettaForum
+
+
+## Install
+
+```bash
+npm install @zetta-forum/language-en-us
+```
---
+++ en_US.js
@@ -0,0 +1,19 @@
+
+export const translations = {
+    "404page": {
+        "page_not_found": "The page you visit does not exist",
+        "please": "Please ",
+        "return": "Return",
+        "or_search_for_related_content": " Or search for related content",
+    },
+    "search-box": {
+        "placeholder": "Search for questions, topics, or people",
+        "search": "Search",
+    },
+}
+
+export default translations
+
+if (top && top.ZettaForum) {
+    top.ZettaForum.addTranslations("en_US", translations)
+}
